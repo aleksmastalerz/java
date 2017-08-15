42ff30bd97bda041d4a79b853ffba46dc55c53b5
Aleks <aleks.mastalerz@gmail.com>
2017-08-15 16:45:10 +0200

    10.3

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,14 @@
-span.each(function(index, element) {
-	if(index % 2 == 0) {
-		$(element).css('color', 'red');
-	};
+var carouselList = $('#carousel ul');
+$(function(){ 
+  setInterval(changeSlides, 3000);
+  carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
 });
-
-var paragraphs = $('p');
-paragraphs.each(function(index, element) {
-
-    var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
-    $(element).append(button) 
-
-});
-
-$('button').click(function(){
-	alert($(this).attr('data-tmp'));
-});
+function changeSlides() {
+  carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+};
+function moveFirstSlide() {
+  var firstItem = carouselList.find('li:first');
+  var lastItem = carouselList.find('li:last');
+  lastItem.after(firstItem);
+  carouselList.css({marginLeft:0});
+};
