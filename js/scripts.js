6ddaaa359a395cfd7f2f1149ee57710c0e3bc4a9
Aleks <aleks.mastalerz@gmail.com>
2017-08-15 16:20:09 +0200

    9.1

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,14 @@
-function getTriangleArea(a, h) {
-	var whatIsBigger = a > 0 ? '' : 'nieprawidłowe dane';
-	var whatIsBigger = h > 0 ? '' : 'nieprawidłowe dane';
-
-return a*h/2;
+function getTriangleArea (a, h) {
+  if (a <= 0 || h <= 0) {
+    console.log('wrong values');
+    return 'podałeś błędne dane';
+  } else {
+    return (a * h / 2);
+  }
 }
-console.log( getTriangleArea(10,6) )
+
+var triangle1Area = getTriangleArea(5, 10);
+var triangle2Area = getTriangleArea(-5, 20);
+var triangle3Area = getTriangleArea(17, 26);
+
+console.log(triangle1Area, triangle2Area, triangle3Area)
