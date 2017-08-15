a59d67b18e3cb5848e8197fc905488675e2b9e16
Aleks <aleks.mastalerz@gmail.com>
2017-08-15 16:21:28 +0200

    9.2

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,22 @@
-function getTriangleArea(a, h) {
-	var whatIsBigger = a > 0 ? '' : 'nieprawidłowe dane';
-	var whatIsBigger = h > 0 ? '' : 'nieprawidłowe dane';
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
 
-return a*h/2;
+console.log(femaleNames);
+console.log(maleNames);
+
+var allNames = femaleNames.concat(maleNames);
+
+console.log(allNames);
+
+var newName = 'Marian';
+
+if (allNames.indexOf(newName) === -1) {
+  allNames.push(newName);
+  var indexOfMarian1 = allNames.indexOf('Marian');
+  console.log('Imię "Marian" zostało dodane do tablicy na pozycji ' + indexOfMarian1 + ' (pamiętaj liczenie rozpoczynamy od zera)');
+} else {
+  var indexOfMarian2 = allNames.indexOf('Marian');
+  console.log('Imię "Marian" znajduje się już w tablicy na pozycji ' + indexOfMarian2 + ' (pamiętaj liczenie rozpoczynamy od zera)');
 }
-console.log( getTriangleArea(10,6) )
+
+console.log(allNames);
